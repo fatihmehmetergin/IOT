2ebd03b499fb66ff97dfe968eb370e03fb3f8b24
ahmetse <senolahmet@gmail.com>
2020-01-18 11:44:50 +0300

    Add files via upload

---
+++ adafruit_sicaklikNemPublish.py
@@ -0,0 +1,27 @@
+from gpiozero import LED
+import paho.mqtt.client as mqtt
+from time import sleep
+import sys
+import Adafruit_DHT
+led = LED (21)
+def baglanti_saglandiginda(client, userdata, flags, rc):
+    print ('Baglandi, rc' + str(rc))
+def mesaj_geldiginde(client, userdata, msg):
+    mesaj = msg.payload
+def onPublish(client, userdata, result):
+    print ('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity), '  published')
+istemci = mqtt.Client()
+istemci.on_connect = baglanti_saglandiginda
+istemci.on_message = mesaj_geldiginde
+istemci.on_publish = onPublish
+istemci.username_pw_set("fatihmehmetergin", "aio_Lute27qoTTO9IteH5mlGLAlzL4QC") 
+istemci.connect("io.adafruit.com", port = 1883)
+while True:
+    istemci.loop_start()
+    humidity, temperature =Adafruit_DHT.read_retry(11, 2)
+    istemci.publish('fatihmehmetergin/feeds/sicaklik', temperature)
+    istemci.publish('fatihmehmetergin/feeds/nem', humidity)
+    led.on()
+    sleep(10)
+    led.off()
+    istemci.loop_stop()# publish metodu kullanilirken loop_forever() kullanilamaz
